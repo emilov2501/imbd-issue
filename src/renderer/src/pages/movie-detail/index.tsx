22e1927a1c8bccc1d1684f8e29adfda626f4ff14
Eldiiar <emilov2501@gmail.com>
2024-03-11 10:17:10 +0600

    fix: add movie-detail page

---
+++ src/renderer/src/pages/movie-detail/index.tsx
@@ -0,0 +1,82 @@
+import ArrowBack from '@mui/icons-material/ArrowBack'
+import { AppBar, Box, Grid, IconButton, Toolbar, Typography } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
+import CssBaseline from '@mui/material/CssBaseline'
+import { MovieInfoBlock, MoviePoster, movieModel } from '@renderer/entities/movie'
+import compose from 'compose-function'
+import { useNavigate } from 'react-router-dom'
+import { WithLoadMovie } from './lib/hoc/with-load-movie'
+
+const MovieDetail = () => {
+  const loading = movieModel.useLoading()
+  const movie = movieModel.useMovie()
+  const r = useNavigate()
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        sx={{
+          background: 'black'
+        }}
+        component="nav"
+        position="static"
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={() => r(-1)}
+          >
+            <ArrowBack />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            {movie?.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          marginBlock: '15px',
+          paddingInline: '15px',
+          boxSizing: 'border-box'
+        }}
+      >
+        <Grid container boxSizing="border-box" columnGap={2} rowGap={2}>
+          <Grid lg={4}>
+            <MoviePoster />
+          </Grid>
+          <Grid lg>
+            <MovieInfoBlock />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  )
+}
+
+export default compose(WithLoadMovie)(MovieDetail)
---
+++ src/renderer/src/pages/movie-detail/lib/hoc/with-load-movie.tsx
@@ -0,0 +1,46 @@
+import { movieModel } from '@renderer/entities/movie'
+import { http } from '@renderer/shared/api'
+import { useQuery } from '@tanstack/react-query'
+import get from 'lodash.get'
+import { ComponentType } from 'react'
+import { useParams } from 'react-router-dom'
+
+export const WithLoadMovie = (Component: ComponentType) => {
+  const _MovieDetail = () => {
+    const params = useParams()
+    const { data, isSuccess, isLoading } = useQuery({
+      queryKey: movieModel.movieKeys.queries.detail(get(params, 'id', '')),
+      queryFn: async ({ queryKey }) => {
+        const [_, tt] = queryKey
+        const { data } = await http({
+          method: 'get',
+          params: {
+            tt
+          }
+        })
+
+        return data
+      }
+    })
+
+    movieModel.setLoading(isLoading)
+
+    if (isSuccess) {
+      const poster = get(data, 'fake.#IMG_POSTER', '')
+      const actors = get(data, 'fake.#ACTORS', '')
+      const reviews = get(data, 'main.reviews.total', 0)
+      movieModel.addMovie(
+        movieModel.mapMovie({
+          ...get(data, 'short'),
+          poster,
+          actors,
+          reviews
+        })
+      )
+    }
+
+    return <Component />
+  }
+
+  return _MovieDetail
+}
